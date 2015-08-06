0ee1b043f0ec0ede4401354eaa83ae3bd5c7b912
Andy Shora <Andy.Shora@rga.com>
2015-08-06 16:09:26 +0100

    Cube of 10k particles

--- app/js/main.js
+++ app/js/main.js
@@ -13,96 +13,70 @@ window.requestAnimFrame = (function(){
     };
   })();
 
-var NUM_PARTICLES = 100;
+var NUM_PARTICLES = 10000;
 
 // set the scene size
-var WIDTH = 800,
-    HEIGHT = 600;
+var WIDTH = window.innerWidth,
+    HEIGHT = window.innerHeight;
 
 // set some camera attributes
-var VIEW_ANGLE = 45,
-    ASPECT = WIDTH / HEIGHT,
-    NEAR = 0.1,
+var VIEW_ANGLE = 60,
+    NEAR = 1,
     FAR = 1000;
 
-// create a WebGL renderer, camera, and a scene
-var scene = new THREE.Scene();
-var renderer = new THREE.WebGLRenderer();
-var camera = new THREE.PerspectiveCamera( 60, window.innerWidth / window.innerHeight, 1, 1000 );
+var scene, renderer, camera, controls;
 
-camera.position.z = 500;
+initWorld();
+animate();
+render();
 
-// the camera starts at 0,0,0 so pull it back
-camera.position.z = 5;
+function initWorld() {
 
-var controls = new THREE.OrbitControls( camera );
-controls.addEventListener('change', render);
+  // create a WebGL renderer, camera, and a scene
+  scene = new THREE.Scene();
+  renderer = new THREE.WebGLRenderer();
+  camera = new THREE.PerspectiveCamera(VIEW_ANGLE, window.innerWidth / window.innerHeight, NEAR, FAR);
 
-scene.add(camera);
+  // the camera starts at 0,0,0 so pull it back
+  camera.position.z = 1000;
 
-// start the renderer - set the clear colour
-// to a full black
-renderer.setClearColor(new THREE.Color(0, 1));
-renderer.setSize(WIDTH, HEIGHT);
+  controls = new THREE.OrbitControls( camera );
+  controls.addEventListener('change', render);
 
-// create the particle variables
-var pointCloudGeometry = new THREE.Geometry();
 
-var pointCloudMaterial = new THREE.PointCloudMaterial({
-  color: 0xffffff,
-  size: 1
-});
+  // start the renderer - set a colour with full opacity
+  renderer.setClearColor(new THREE.Color(0, 1));
+  renderer.setSize(WIDTH, HEIGHT);
 
-// now create the individual particles
-for(var p = 0; p < NUM_PARTICLES; p++) {
+  // create the particle variables
+  var pointCloudGeometry = new THREE.Geometry();
+  var pointCloudMaterial = new THREE.PointCloudMaterial({
+    color: 0xffffff,
+    size: 10,
+    fog: true
+  });
 
-  // create a particle with random
-  // position values, -250 -> 250
-  var pX = Math.random() * 500 - 250,
-      pY = Math.random() * 500 - 250,
-      pZ = Math.random() * 500 - 250;
+  // now create the individual particles
+  for(var p = 0; p < NUM_PARTICLES; p++) {
 
-  // add it to the particle system
-  pointCloudGeometry.vertices.push(new THREE.Vector3(pX, pY, pZ));
+    // create a particle with random position values, -250 -> 250
+    var pX = Math.random() * 500 - 250,
+        pY = Math.random() * 500 - 250,
+        pZ = Math.random() * 500 - 250;
+
+    // add it to the particle system
+    pointCloudGeometry.vertices.push(new THREE.Vector3(pX, pY, pZ));
+  }
+
+  // create the particle system
+  var pointCloud = new THREE.PointCloud(pointCloudGeometry, pointCloudMaterial);
+
+  scene.add(pointCloud);
+  scene.add(camera);
+
+  window.addEventListener( 'resize', onWindowResize, false );
 }
 
-// create the particle system
-var pointCloud = new THREE.PointCloud(pointCloudGeometry, pointCloudMaterial);
-
-pointCloud.sortParticles = true;
-
-// flag that we've changed the evrtices
-pointCloud.geometry.__dirtyVertices = true;
-
-console.log(pointCloud);
-
-// add it to the scene
-scene.add(pointCloud);
-
-
-
-
-
-var squareGeometry = new THREE.Geometry();
-squareGeometry.vertices.push(new THREE.Vector3(-1.0,  1.0, 0.0));
-squareGeometry.vertices.push(new THREE.Vector3( 1.0,  1.0, 0.0));
-squareGeometry.vertices.push(new THREE.Vector3( 1.0, -1.0, 0.0));
-squareGeometry.vertices.push(new THREE.Vector3(-1.0, -1.0, 0.0));
-squareGeometry.faces.push(new THREE.Face3(0, 1, 2));
-squareGeometry.faces.push(new THREE.Face3(0, 2, 3));
-
-var squareMaterial = new THREE.MeshBasicMaterial({
-  color:0xFFFF00,
-  side:THREE.DoubleSide
-});
-
-var squareMesh = new THREE.Mesh(squareGeometry, squareMaterial);
-squareMesh.position.set(0.5, 0.0, 4.0);
-scene.add(squareMesh);
-
-window.addEventListener( 'resize', onWindowResize, false );
-
-animate();
 
 function onWindowResize() {
 
